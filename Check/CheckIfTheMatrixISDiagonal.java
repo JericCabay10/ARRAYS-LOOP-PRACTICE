7bb204cbd2806b5fe8f97f20256e3485ebbcc002
JericCabay10 <jericcabay10@gmail.com>
2025-10-31 06:16:59 +0000

    Update

--- Check/CheckIfTheMatrixISDiagonal.java
+++ Check/CheckIfTheMatrixISDiagonal.java
@@ -2,7 +2,8 @@ package Check;
 
 public class CheckIfTheMatrixISDiagonal {
     public static void main(String[] args) {
-        int[][] matrix = {{1, 0, 0}, {0, 5, 0}, {0, 0, 9}};
+        // --int[][] matrix = {{1, 0, 0}, {0, 5, 0}, {0, 0, 9}};
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
         for(int row = 0; row < matrix.length; row++) {
             for(int col = 0; col < matrix[row].length; col++) {
@@ -10,15 +11,23 @@ public class CheckIfTheMatrixISDiagonal {
             }
             System.out.println();
         }
-
+        System.out.println();
         
+        boolean isDiagonal = true;
         for(int row = 0; row < matrix.length; row++) {
-            boolean isDiagonal = true;
             for(int col = 0; col < matrix[row].length; col++) {
-                if
+                if(row != col && matrix[row][col] != 0) {
+                    isDiagonal = false;
+                    break;
+                }
             }
             System.out.println();
         }
-        
+
+        if(isDiagonal) {
+            System.out.println("The Matrix is diagonal");
+        }else {
+            System.out.println("The matrix is not a diagonal.");
+        }
     }
 }
